8ef54974a8866172429775c19a6312a9bc642652
rcoburn <rcoburn@airspringsoftware.com>
2018-02-17 18:10:39 -0500

    so many things

--- res/scripts/app.js
+++ res/scripts/app.js
@@ -1,29 +1,52 @@
-$(function () {
-    $('img').bind('contextmenu', function (e) {
-        return false;
-    });
-});
+ var panelsList = {
+     "panels": [{
+             "panelName": "episode 1 - cover ",
+             "panelLink": "/res/panels/volume1/v1-e1-cover.html"
+                },
+        /* {
+            "panelName": "episode 1 - panel 1",
+             "panelLink": "/res/panels/volume1/v1-e1-p1.html"
+                } */
+         ]
+ };
 
-$(document).ready(function () {
+ $(document).ready(function () {
+     var listItems = '<option selected="selected" value="0">- Select -</option>';
+     for (var i = 0; i < panelsList.panels.length; i++) {
+         listItems += "<option value='" + panelsList.panels[i].panelLink + "'>" + panelsList.panels[i].panelName + "</option>";
+     }
 
-    $(".nav-btn").click(function (event) {
-        $("#nav").animate({
-            "right": "0px"
-        }, "slow").addClass('visible');
 
-    });
+     $("#dropdown").html(listItems);
 
-    $(".nav-close").click(function (event) {
-        $("#nav").animate({
-            "right": "-9999px"
-        }, "slow").removeClass('visible');
+     $(function () {
+         $('#dropdown').on('change', function () {
+             var url = $(this).val(); // get selected value
+             if (url) { // require a URL
+                 window.location = url; // redirect
+             }
+             return false;
+         });
+     });
 
-    });
+     $(".nav-btn").click(function (event) {
+         $("#nav").animate({
+             "right": "0px"
+         }, "slow").addClass('visible');
 
-    $(".panel").click(function (event) {
-        $(this).toggleClass('fullsize');
-        $(this).toggleClass('modal');
-        $("body").toggleClass('modal-open');
-    });
+     });
 
-});
+     $(".nav-close").click(function (event) {
+         $("#nav").animate({
+             "right": "-9999px"
+         }, "slow").removeClass('visible');
+
+     });
+
+     $(".panel").click(function (event) {
+         $(this).toggleClass('fullsize');
+         $(this).toggleClass('modal');
+         $("body").toggleClass('modal-open');
+     });
+
+ });

5de575b536742293cd1675f0426e86a68954503d
rcoburn <rcoburn@airspringsoftware.com>
2018-02-25 20:09:31 -0500

    added panel 2

--- res/scripts/app.js
+++ res/scripts/app.js
@@ -6,6 +6,10 @@
          {
              "panelName": "episode 1 - panel 1",
              "panelLink": "/res/panels/volume1/v1-e1-p1.html"
+                },
+         {
+             "panelName": "episode 1 - panel 2",
+             "panelLink": "/res/panels/volume1/v1-e1-p2.html"
                 }
          ]
  };

3233e06a21a243b17418bba17adaa75e985ea6d5
Roxanne Coburn <roxanne@Roxannes-MacBook-Pro.local>
2018-09-20 23:14:25 -0400

    fucking finally

--- res/scripts/app.js
+++ res/scripts/app.js
@@ -22,6 +22,10 @@
          {
              "panelName": "episode 1 - panel 5",
              "panelLink": "/res/panels/volume1/v1-e1-p5.html"
+                },
+                 {
+             "panelName": "episode 1 - panel 6",
+             "panelLink": "/res/panels/volume1/v1-e1-p6.html"
                 }
          ]
  };
